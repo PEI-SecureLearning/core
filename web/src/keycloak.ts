33019853d6f09916cbf22f25ae82d5225974804c
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-12-03 11:00:50 +0000

    role-based sidebar

--- web/src/keycloak.ts
+++ web/src/keycloak.ts
@@ -3,8 +3,6 @@ import Keycloak from 'keycloak-js'
 const isAdminRoute = window.location.pathname.startsWith("/admin");
 const userRealm = localStorage.getItem('user_realm');
 
-// If we are not on admin route and have no user realm, we can't initialize Keycloak properly yet.
-// We will handle this in main.tsx by checking if we have a valid config.
 const realm = isAdminRoute ? 'master' : (userRealm || 'placeholder');
 const clientId = isAdminRoute ? 'react-admin' : 'react-app';
 
