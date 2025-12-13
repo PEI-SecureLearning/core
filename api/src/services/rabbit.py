ec266c38b3fa72ace2709798e16be8e79abedaa5
ixsb <igorbaltarejo05@gmail.com>
2025-12-13 12:58:18 +0000

    Hotfix to rabbitmq api service

--- api/src/services/rabbit.py
+++ api/src/services/rabbit.py
@@ -6,7 +6,7 @@ class RabbitMQService:
     def __init__(self):
         self.connection = pika.BlockingConnection(settings.RABBITMQ_CONNECTION_PARAMS)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=settings.RABBITMQ_QUEUE)
+        self.channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
 
     def send_email(self, email: RabbitMQEmailMessage):
         self.channel.basic_publish(exchange='', routing_key=settings.RABBITMQ_QUEUE, body=email.model_dump_json())
