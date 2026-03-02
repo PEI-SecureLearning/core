58340cf5939ed9a61775e9c520d3aa1abbe988c1
GabrielFcGoncalves <gabfacgon3@gmail.com>
2026-03-02 15:24:46 +0000

    feature/ Making tests independent + new e2e workflow(probably does no… (#68)
    * feature/ Making tests independent + new e2e workflow(probably does not work)
    
    * fix/ separate workflows
    
    * refining the yml
    
    * feature/ addedworkers to playwright

--- web/src/components/AdminPanel.tsx
+++ web/src/components/AdminPanel.tsx
@@ -3,7 +3,7 @@ import { useKeycloak } from '@react-keycloak/web'
 import { useNavigate } from '@tanstack/react-router'
 import { TenantForm } from './admin/TenantForm'
 import { PreviewPanel } from './admin/PreviewPanel'
-import { toast } from 'sonner'	
+import { toast } from 'sonner'
 
 export function CreateTenantPage() {
     const { keycloak } = useKeycloak()
